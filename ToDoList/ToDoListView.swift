8e92c31cde2aeef231d63a6a064b05e2d752ae7b
Philip Keller <philipjoelkeller@gmail.com>
2023-02-17 23:33:03 -0800

    Programatic Navigation with an Array - YouTube 5.2.1

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift",
+                 "Build Apps",
+                 "Change the World",
+                 "Bring the Awesome",
+                 "Take a Vaction"]
+    
     var body: some View {
         NavigationStack {
             List {
-                ForEach(0..<100, id: \.self) { number in
+                ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: "Item \(number+1)")
+                        DetailView(passedValue: toDo)
                     } label: {
-                        Text("Item \(number+1)")
+                        Text(toDo)
                     }
                 }
             }
-            .navigationTitle("School Year")
+            .navigationTitle("To Do List")
             .listStyle(.plain)
         }
     }
