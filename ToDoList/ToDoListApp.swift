69d27a7b42e749cd68b99ab9579bc9a290bdd384
Philip Keller <philipjoelkeller@gmail.com>
2023-02-17 22:38:21 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Philip Keller on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
